cf7b3e54351974b47efcb34442c05f4345c6d1f9
mudimut89 <mudimu19t@gmail.com>
2026-04-10 20:17:00 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -36,10 +36,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "http://localhost:3000",
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
+        "https://medical-image-segmentation-for-brai.vercel.app",
+        "https://medical-image-segmentation-for-brain-tumor-prognosis-axo5wj8go.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
